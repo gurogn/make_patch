96a7db8d445595c81ce518b2a8815a9e206f4c59
gurogn <36068699+gurogn@users.noreply.github.com>
2020-09-01 18:40:55 +0800

    Update make_patch.sh

--- make_patch.sh
+++ make_patch.sh
@@ -21,7 +21,7 @@ PATCH_FOLDER=""
 create_patch_dir()
 {
 	curDateTime=$(date "+%Y-%m-%d_%H_%M_%S")
-	PATCH_FOLDER=~//new_old_patch//$curDateTime
+	PATCH_FOLDER=.//$curDateTime
 	mkdir "$PATCH_FOLDER"
 }
 ARCHIVE=n
@@ -48,9 +48,9 @@ while getopts :adh opt;do
 		;;
 	esac
 done
-if [ ! -d ~/new_old_patch ];then
-mkdir ~/new_old_patch
-fi
+#if [ ! -d ~/new_old_patch ];then
+#mkdir ~/new_old_patch
+#fi
 #echo $curDateTime
 #to create the patch dir
 create_patch_dir
@@ -59,49 +59,52 @@ shift $SHIFT_NUM
 fi
 if [ $CID_NUM == 0 ] ; then 
 	echo "Generating HEAD commit diff"
-	archive_files=$(git diff --name-only HEAD^)
+	archive_files=$(git diff --diff-filter=ACMR --name-only HEAD^)
 	if [[ -z $archive_files ]]
 	then 	
 	echo "The HEAD maybe a tag, there is no diff files, please try another CID"
 	exit 1
 	fi
-	status=$(git archive -o ~/new_old_patch/new.zip HEAD $archive_files 2>&1)
+	status=$(git archive -o ./new.zip HEAD $archive_files 2>&1)
 	check_err "$status"	
-	git archive -o ~/new_old_patch/old.zip HEAD^ $archive_files
+	archive_files=$(git diff --diff-filter=ACMR --name-only HEAD HEAD^)
+	git archive -o ./old.zip HEAD^ $archive_files
 	if [ $DIFF == "y" ] ; then
 		git diff HEAD^ >> $PATCH_FOLDER/$curDateTime.diff
 	fi 
 elif [ $CID_NUM == 1 ] ; then
 	echo "Generating the diff of commit id $1"
-	archive_files=$(git diff --name-only $1 $1^)
+	archive_files=$(git diff --diff-filter=ACMR --name-only $1^ $1)
 	if [[ -z $archive_files ]]
 	then 	
 	echo "The CID maybe a tag, there is no diff files, please try another CID"
 	exit 1
 	fi
-	status=$(git archive -o ~/new_old_patch/new.zip $1 $archive_files 2>&1)
-	check_err "$status"
-	git archive -o ~/new_old_patch/old.zip $1^ $archive_files 
+	status=$(git archive -o ./new.zip $1 $archive_files 2>&1)
+	check_err "$status"	
+	archive_files=$(git diff --diff-filter=ACMR --name-only $1 $1^)	
+	git archive -o ./old.zip $1^ $archive_files 
 	if [ $DIFF == "y" ] ; then
 		git diff $1^ $1 >> $PATCH_FOLDER/$curDateTime.diff
 	fi 
 elif [ $CID_NUM == 2 ] ; then
 	echo "Generating the diff of commit id $1 $2"
-	archive_files=$(git diff --name-only $1 $2)
+	archive_files=$(git diff --diff-filter=ACMR --name-only $2 $1)
 	if [[ -z $archive_files ]]
 	then 	
 	echo "One CID maybe a tag, there is no diff files, please try another CID"
 	exit 1
 	fi
-	status=$(git archive -o ~/new_old_patch/new.zip $1 $archive_files 2>&1)
+	status=$(git archive -o ./new.zip $1 $archive_files 2>&1)
 	check_err "$status"
-	status=$(git archive -o ~/new_old_patch/old.zip $2 $archive_files 2>&1)
+	archive_files=$(git diff --diff-filter=ACMR --name-only $1 $2)
+	status=$(git archive -o ./old.zip $2 $archive_files 2>&1)
 	check_err "$status"
 	if [ $DIFF == "y" ] ; then
 		git diff $2 $1 >> $PATCH_FOLDER/$curDateTime.diff
 	fi 
 fi
-cd ~/new_old_patch
+#cd ~/new_old_patch
 #echo $PWD
 unzip -o -d ./$curDateTime/new/ new.zip
 unzip -o -d ./$curDateTime/old/ old.zip
